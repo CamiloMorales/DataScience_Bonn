a179859fc3db3091090da5a3c7e9a6d69982656d
“CamiloMorales” <camilo.morales@gmail.com>
2016-06-26 07:01:42 +0200

    Includes changes in MLP model.

--- ToolBox/Helper.py
+++ ToolBox/Helper.py
@@ -8,8 +8,8 @@ import numpy as np
 
 # In[1]:
 
-def convert_one_hot_encoding(y):
-    oneHot = np.zeros([y.shape[0], 6])
+def convert_one_hot_encoding(y, number_of_classes):
+    oneHot = np.zeros([y.shape[0], number_of_classes])
     for idx, l in enumerate(y):
         oneHot[idx, l] = 1
     return oneHot
