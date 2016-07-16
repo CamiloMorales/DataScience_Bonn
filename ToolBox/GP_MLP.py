e4bc8307ae7e9be8335a610e3664b04cf7e05a3d
“CamiloMorales” <camilo.morales@gmail.com>
2016-07-16 04:57:49 +0200

    Add fixes on GP_MLP.

--- ToolBox/GP_MLP.py
+++ ToolBox/GP_MLP.py
@@ -5,15 +5,17 @@
 
 import numpy as np
 import operator
+import time
 
 import Individual, Helper, MLP
 
 
-# In[2]:
+# In[1]:
 
 def init(gp_config, train_set, val_set):
-    population = []
+    
     def create_population():
+        population = []
         for individual_number in range(gp_config["population-size"]):
             number_of_layers = np.random.randint(gp_config["min-layers"], gp_config["max-layers"] + 1)
             current_individual = []
@@ -26,12 +28,13 @@ def init(gp_config, train_set, val_set):
             new_individual = Individual.Individual()
             new_individual._individual = current_individual
             population.append(new_individual)
+        return population
             
-    def evaluate_fitness(start_from):
+    def evaluate_fitness(start_from, population):
         count = 0
-        for ind in population:
-            print "Evaluating fitness " + str(count+1) + " of " + str(len(population))
-            ind_array = ind.individual
+        for index in xrange(start_from, len(population)):
+            print "Evaluating fitness " + str(index) + " of " + str(len(population))
+            ind_array = population[index].individual
             layers = []
             for layer in xrange(start_from, len(ind_array), 2):
                 layer_params = {}
@@ -44,10 +47,11 @@ def init(gp_config, train_set, val_set):
             mlp['train'](train_set)
             predicted = mlp['predict'](val_set['x'])
             #ind.fitness = Helper.estimate_cross_entropy(val_set['y'], predicted)
-            ind._fitness = np.sum(np.multiply(val_set['y'],np.log(predicted + 0.0))) * (-1.0/val_set['y'].shape[0])       
-            print "ind._fitness: "+str(ind._fitness)
+            population[index]._fitness = np.sum(np.multiply(val_set['y'],np.log(predicted + 0.0))) * (-1.0/val_set['y'].shape[0])       
+            print "ind._fitness: "+str(population[index]._fitness)
             count+=1
-            
+        return population
+    
     def do_cross_over(parent1, parent2):
         
         def do_cross_over(index, first_parent, second_parent):
@@ -56,7 +60,9 @@ def init(gp_config, train_set, val_set):
                 child.append(first_parent[i])
             for i in xrange(index, len(second_parent)):
                 child.append(second_parent[i])
-            return child
+            new_individual = Individual.Individual()
+            new_individual._individual = child
+            return new_individual
         
         length = 0
         if len(parent1) < len(parent1):
@@ -76,33 +82,36 @@ def init(gp_config, train_set, val_set):
         return children
     
     def do_mutation(individual):
-        rand = np.random.randint(0, len(individual))
+        print "Do Mutation (length): " + str(len(individual.individual))
+        rand = np.random.randint(0, len(individual.individual))
         if rand % 2 == 0:
-            individual[rand] = np.random.randint(gp_config['min-units'], gp_config['max-units'])
+            individual.individual[rand] = np.random.randint(gp_config['min-units'], gp_config['max-units'])
         else:
-            individual[rand] = np.random.randint(0, len(gp_config['functions']))
+            individual.individual[rand] = np.random.randint(0, len(gp_config['functions']))
         
     
     def train():
-        create_population()
+        population = create_population()
         start_from = 0
         for step in xrange(0, gp_config['max-steps']):
+            start_time = time.time()
             print "Training step: " + str(step)
-            evaluate_fitness(start_from)
+            population = evaluate_fitness(start_from, population)
             population.sort(key=operator.attrgetter('fitness'))
             print 'step:' + str(step) + ' fitness:' +str(population[0].fitness) 
             for i in xrange(0, gp_config['population-size'] - gp_config['survival-size']):
                 del population[-1]
             for i in xrange(0, int((gp_config['population-size'] - gp_config['survival-size'])/2)):
-                p1 = population[np.random.randint(0, gp_config['survival-size'])].get_individual()
-                p2 = population[np.random.randint(0, gp_config['survival-size'])].get_individual()
+                p1 = population[np.random.randint(0, gp_config['survival-size'])]._individual
+                p2 = population[np.random.randint(0, gp_config['survival-size'])]._individual
                 children = do_cross_over(p1,p2)
                 for child in children:
                     do_mutation(child)
                 population += children
             start_from = gp_config['survival-size']
-        population.sort(key=operator.attrgetter('fitness'))     
-        return population[0].individual
+        population.sort(key=operator.attrgetter('fitness'))  
+        print("Step: "+str(step)+ " took: " + str((time.time() - start_time)/60) + " minutes")
+        return population[0]._individual
     return {
         'train':train
     }
